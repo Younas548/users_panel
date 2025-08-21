27a41f483f4efb255f9425c43dc91fb640919049
Muhammad Ali <aliyounas343250@gmail.com>
2025-08-21 13:30:24 +0300

    new updatess

--- lib/core/constants/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const primary = Color(0xFF2B7FFF);
-  static const success = Color(0xFF23C55E);
-  static const warning = Color(0xFFF59E0B);
-  static const danger  = Color(0xFFEF4444);
-  static const textSecondary = Color(0xFF6B7280);
-}
